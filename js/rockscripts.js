24ae0bcb987f0ca36ca3bbdcda267ac41c595294
Anatoliy Klots and Jason Lazzuri <jason@>
2017-04-11 16:16:45 -0700

    link pages to other pages

---
+++ js/rockscripts.js
@@ -0,0 +1,42 @@
+//Business Logic
+
+var humanChoice;
+
+function turn(humanChoice) {
+  var randomAI =  Math.floor((Math.random()* 3) +1);
+  var aiChoice;
+  if (randomAI === 1) {
+      aiChoice = "rock";
+      alert(aiChoice);
+    } else if (randomAI === 2) {
+      aiChoice = "paper";
+      alert(aiChoice);
+    } else {
+      aiChoice = "scissors";
+      alert(aiChoice);
+    }
+
+    console.log(aiChoice, humanChoice)
+  if (humanChoice === aiChoice) {
+    alert('draw')
+  } else if (humanChoice === 'rock' && aiChoice === 'paper') {
+    alert ('ai won');
+  } else if (humanChoice === 'paper' && aiChoice === 'scissors') {
+    alert ('ai won');
+  }
+  else if (humanChoice === 'scissors' && aiChoice === 'rock') {
+  alert ('ai won');
+ } else {
+    alert('human won');
+  }
+
+}
+
+
+//UI
+$(document).ready(function(){
+  $('.btn').click(function(){
+  var humanChoice = $(this).attr('id');
+  turn(humanChoice);
+ });
+});
