5eae7e6423ce4b186b006ee491a3a1698148b12e
Anatoliy Klots and Jason Lazzuri <anatoliy@>
2017-04-12 10:06:35 -0700

    add pictures and buttons to page 4

--- js/rockscripts.js
+++ js/rockscripts.js
@@ -1,36 +1,65 @@
 //Business Logic
 
 var humanChoice;
+var aiTotalWin = 0;
+var playerTotalWin = 0;
 
 function turn(humanChoice) {
   var randomAI =  Math.floor((Math.random()* 3) +1);
   var aiChoice;
   if (randomAI === 1) {
-      aiChoice = "rock";
-      alert(aiChoice);
-      $()
+      aiChoice = "skull";
+      $('#ai').show();
+      $('#ai1').append('<li>' + aiChoice + '</li>');
     } else if (randomAI === 2) {
-      aiChoice = "paper";
-      alert(aiChoice);
+      aiChoice = "parchment";
+      $('#ai').show();
+      $('#ai1').append('<li>' + aiChoice + '</li>');
     } else {
-      aiChoice = "scissors";
-      alert(aiChoice);
+      aiChoice = "knife";
+      $('#ai').show();
+      $('#ai1').append('<li>' + aiChoice + '</li>');
     }
 
     console.log(aiChoice, humanChoice)
   if (humanChoice === aiChoice) {
-    alert('draw')
-  } else if (humanChoice === 'rock' && aiChoice === 'paper') {
-    alert ('ai won');
-  } else if (humanChoice === 'paper' && aiChoice === 'scissors') {
-    alert ('ai won');
-  }
-  else if (humanChoice === 'scissors' && aiChoice === 'rock') {
-  alert ('ai won');
- } else {
-    alert('human won');
-  }
+    $('#player').show();
+    $('#player1').append('<li>' + humanChoice + '</li>');
+  } else if (humanChoice === 'skull' && aiChoice === 'parchment') {
+    $('#player').show();
+    $('#player1').append('<li>' + humanChoice + '</li>');
+    aiTotalWin += 1;
+    if (aiTotalWin >= 3) {
+      $("#main").hide();
+      $('#ai-won').show();
+    }
 
+  } else if (humanChoice === 'parchment' && aiChoice === 'knife') {
+    $('#player').show();
+    $('#player1').append('<li>' + humanChoice + '</li>');
+    aiTotalWin += 1;
+    if (aiTotalWin >= 3) {
+      $("#main").hide();
+      $('#ai-won').show();
+    }
+  }
+  else if (humanChoice === 'knife' && aiChoice === 'skull') {
+  $('#player').show();
+  $('#player1').append('<li>' + humanChoice + '</li>');
+    aiTotalWin += 1;
+    if (aiTotalWin >= 3) {
+      $("#main").hide();
+      $('#ai-won').show();
+    }
+ } else {
+    $('#player').show();
+    $('#player1').append('<li>' + humanChoice + '</li>');
+    playerTotalWin += 1;
+    if (playerTotalWin >= 3) {
+      $('#player-won').show();
+      $("#main").hide();
+    }
+  }
 }
 
 
